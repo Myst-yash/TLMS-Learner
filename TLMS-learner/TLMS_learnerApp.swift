97c00cca7530b0c5aeffb94c233bf1b7f38ed238
Sumit-pd <pdsumit07@gmail.com>
2024-07-03 13:47:10 +0530

    Initial Commit

---
+++ TLMS-learner/TLMS_learnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TLMS_learnerApp.swift
+//  TLMS-learner
+//
+//  Created by Sumit Prasad on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TLMS_learnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TLMS-learnerUITests/TLMS_learnerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TLMS_learnerUITestsLaunchTests.swift
+//  TLMS-learnerUITests
+//
+//  Created by Sumit Prasad on 03/07/24.
+//
+
+import XCTest
+
+final class TLMS_learnerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
