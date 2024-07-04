b7d5fd773ccc1e7b5fc5c434e8a33bae5c235d9e
Ishika Chaudhary <ishikachaudhary0812@gmail.com>
2024-07-04 18:03:54 +0530

    LogInScreenUpdate

--- TLMS-learner/learnerlogin.swift
+++ TLMS-learner/learnerlogin.swift
@@ -5,63 +5,77 @@
 //  Created by Sumit Prasad on 03/07/24.
 //
 
-import SwiftUI
 
+import SwiftUI
 
 struct ContentView: View {
     @State private var email = ""
     @State private var password = ""
     @State private var showAlert = false
-
+    
     var body: some View {
         NavigationView {
-            ZStack(alignment: .topLeading) {
-                
-                Color(.systemGray6)
+            ZStack(alignment:.topLeading) {
+                Color(UIColor(hex: "#FFFFFF")!)
                     .edgesIgnoringSafeArea(.all)
-
-                VStack {
-//
-                    HStack {
-                            Text("Welcome to Svadhyaya")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .padding()
-                            Spacer()
-                                      }
-                                  
-                            Spacer()
-                            .frame(height: 20)
+                Image("Waves")
+                    .resizable()
+                    .scaledToFit()
+                    .position(x:197,y:735)// Extend to ignore safe area insets
                 
+                VStack {
+                    HStack {
+                        
+                    Text("Welcome to Svadhyaya")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding()
+                        
+                    Spacer()
+                }
+            
                     
                     Image("laptop")
-                    
                         .resizable()
                         .scaledToFit()
                         .frame(width: 200, height: 150)
-                        .padding()
-
-                    VStack(spacing: 16) {
+                        .padding(.bottom, 20)
+                    
+                    // Login Form
+                    VStack(spacing: 20) {
                         TextField("Email", text: $email)
-                            .padding()
-                            .background(Color(.systemGray5))
-                            .cornerRadius(8)
-
+                            .padding()                                                     .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+                            .padding(.horizontal)
+                          
+                            
+                        
+                        
                         SecureField("Password", text: $password)
                             .padding()
-                            .background(Color(.systemGray5))
-                            .cornerRadius(8)
+                            .cornerRadius(8)                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
 
+                            .padding(.horizontal)
+                        
+                        HStack {
+                                                    Spacer()
+                                                    Button("Forgot Password") {
+                                                        // Implement forgot password logic here
+                                                    }
+                                                    .foregroundColor(.blue)
+                                                 
+                                                    .padding(.horizontal)
+                                                    .font(.system(size: 15, weight: .bold, design: .default))
+                            
+                                                }
+                                                
+                        
                         Button(action: {
-//                            print("\(email) : \(password)")
-////                            Auth.auth().signIn(withEmail: email, password: password) {
-////                                (fire, error) in if let _ = error {
-////                                    print("Couldnt log in")
-////                        
-//                                }
-//                                print("You've successfully Logged In!!")
-//                            }
-//
                             // Perform login action here
                             showAlert = true
                         }) {
@@ -70,41 +84,66 @@ struct ContentView: View {
                                 .foregroundColor(.white)
                                 .padding()
                                 .frame(maxWidth: .infinity)
-                                .background(Color.purple)
-
-                         
-
+                                .background(Color(UIColor(named: "PrimaryColor")!))
                                 .cornerRadius(8)
                         }
-                        .padding()
-                    }
-                    .padding(.horizontal)
-
-                    HStack {
-                        Text("Don't have an account?")
-                            .font(.headline)
-                            .foregroundColor(.blue)
-//                        NavigationLink(destination: SignupView()) { // Link to signup view
-                            Text("Signup")
-                                .font(.headline)
-                                .foregroundColor(.blue)
+                        .padding(.horizontal)
+                        .alert(isPresented: $showAlert) {
+                            Alert(title: Text("Login Action"), message: Text("Perform your login action here."), dismissButton: .default(Text("OK")))
                         }
+                        
+                        // Sign Up Option
+                        HStack {
+                            Text("Don't have an account?")
+                                .font(.system(size: 15, weight: .regular, design: .default))
+                            NavigationLink(destination: SignupView()) {
+                                Text("Sign up")
+                                    .font(.system(size: 15, weight: .bold, design: .default))
+                                    .fontWeight(.bold)
+                            }
+                        }
+                        
+                        // Social Login Buttons
+                        HStack(spacing: 20) {
+                            Button(action: {
+                                // Perform Google login action
+                                print("Google login")
+                            }) {
+                                Image("Google")
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                  
+                                    .cornerRadius(100)
+                            }
+                            
+                            Button(action: {
+                                // Perform Apple login action
+                                print("Apple login")
+                            }) {
+                                Image("Apple")
+                                    .resizable()
+                                    .frame(width: 50, height:50 )
+                                   
+                                    .cornerRadius(100)
+                            }
+                        }
+                        .padding(.bottom, 20)
                     }
-                    .padding()
+                    .navigationBarHidden(true)
                 }
-                .padding()
             }
-            .navigationTitle("")
-            .navigationBarHidden(true)
-//            .alert("Invalid Credentials", isPresented: $showAlert) {
-//                Button("OK", role: .cancel) { }
-//            } message: {
-//                Text("Please check your email and password.")
-//            }
         }
     }
+}
 
+struct SignupView: View {
+    var body: some View {
+        Text("Signup View")
+    }
+}
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
