2ca9fca5e66e8e3a1aad4d4e5e9ad33e1f84b0e1
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-07-17 15:52:35 +0530

    dynamic search

--- TLMS-learner/SearchBar.swift
+++ TLMS-learner/SearchBar.swift
@@ -9,109 +9,114 @@ import Foundation
 import SwiftUI
 
 struct SearchBar: View {
-    @State var searchText: String = ""
+    @State private var searchText: String = ""
     @Environment(\.presentationMode) var presentationMode
-    var courses: [Course] {
-        popularCourses + recommendedCourses
-    }
-    var filteredCourses: [Course] {
+    var courses: [HomeCourse]
+    
+    var filteredCourses: [HomeCourse] {
         if searchText.isEmpty {
             return courses
         } else {
             return courses.filter { course in
-                course.title.lowercased().contains(searchText.lowercased()) ||
-                course.instructorName.lowercased().contains(searchText.lowercased())
-                
+                course.courseName.lowercased().contains(searchText.lowercased()) ||
+                course.assignedEducator.lowercased().contains(searchText.lowercased())
             }
         }
     }
     
     var body: some View {
-        ZStack {
-            
-            
-            TextField("Search Courses", text: $searchText)
-                .padding(.vertical, 7)
-                .padding(.horizontal, 30)
-                .background(Color(.white))
-                .cornerRadius(8)
-                .overlay(
-                    HStack {
-                        Button(action: {
-                            
-                            presentationMode.wrappedValue.dismiss() // Go back
-                        }) {
-                            Image(systemName: "chevron.left")
-                                .foregroundColor(.blue)
-                                .padding(.leading, 10)
-                            
-                        }
-                        
-                        Spacer()
-                        if !searchText.isEmpty {
+        VStack {
+            ZStack {
+                TextField("Search Courses", text: $searchText)
+                    .padding(.vertical, 7)
+                    .padding(.horizontal, 30)
+                    .background(Color(.white))
+                    .cornerRadius(8)
+                    .overlay(
+                        HStack {
                             Button(action: {
-                                self.searchText = ""
+                                presentationMode.wrappedValue.dismiss() // Go back
                             }) {
-                                Image(systemName: "xmark.circle.fill")
-                                    .foregroundColor(.gray)
+                                Image(systemName: "chevron.left")
+                                    .foregroundColor(.blue)
+                                    .padding(.leading, 10)
                             }
-                        } else {
-                            Image(systemName: "xmark.circle.fill")
-                                .foregroundColor(.white)
                             
+                            Spacer()
+                            if !searchText.isEmpty {
+                                Button(action: {
+                                    self.searchText = ""
+                                }) {
+                                    Image(systemName: "xmark.circle.fill")
+                                        .foregroundColor(.gray)
+                                }
+                            } else {
+                                Image(systemName: "xmark.circle.fill")
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    )
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
+            }
+            .compositingGroup()
+            .shadow(color: .gray, radius: 3)
+            .padding(.bottom, 0)
+            .navigationBarBackButtonHidden()
+            
+            if filteredCourses.isEmpty {
+                VStack {
+                    Text("Uh-oh! No results found 😢")
+                        .opacity(0.5)
+                        .font(.headline)
+                        .padding()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            } else {
+                List(filteredCourses) { course in
+                    HStack {
+                        AsyncImage(url: URL(string: course.courseImage)) { phase in
+                            switch phase {
+                            case .empty:
+                                ProgressView()
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 120)
+                                    .clipped()
+                            case .failure:
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 120)
+                                    .clipped()
+                            @unknown default:
+                                EmptyView()
+                            }
+                        }
+                        VStack(alignment: .leading) {
+                            Text(course.courseName)
+                                .font(.headline)
+                            Text("by \(course.assignedEducator)")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                        Spacer()
+                        Button(action: {
+                            // Action for the arrow button
+                        }) {
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
                         }
                     }
-                )
-            
-                .padding(.leading, 10)
-                .padding(.trailing, 10)
-            
-            
-        }
-        
-        .compositingGroup()
-        .shadow(color: .gray, radius: 3)
-        
-        .padding(.bottom, 0)
-        .navigationBarBackButtonHidden()
-        if filteredCourses.isEmpty {
-            VStack {
-                Text("Uh-oh! No results found 😢")
-                    .opacity(0.5)
-                    .font(.headline)
-                    .padding()
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-        } else {
-            
-            List(filteredCourses) { course in
-                HStack {
-                    Image("\(course.imageName)")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 120)
-                        .clipped()
-                    VStack(alignment: .leading) {
-                        Text(course.title)
-                            .font(.headline)
-                        Text("by \(course.instructorName)")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                    }
-                    Spacer()
-                    Button(action: {
-                        // Action for the arrow button
-                    }) {
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
-                    }
+                    .padding(.vertical, 0)
                 }
-                .padding(.vertical, 0)
             }
         }
     }
 }
 
 #Preview{
-    SearchBar()
+    SearchBar(courses: [])
 }
